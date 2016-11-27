a750b0594d7a10996d24b1ed6f63fa4de47051c5
amnona <amnonim@gmail.com>
2016-11-27 00:21:02 -0600

    search results

--- scdb-website/Site_Main_Flask.py
+++ scdb-website/Site_Main_Flask.py
@@ -1,8 +1,5 @@
-from flask import Blueprint,request,g
-from flask.ext.login import login_required
-from flask.ext.login import current_user
+from flask import Blueprint,request
 import json
-import dbuser
 import requests
 from utils import debug,getdoc
 
@@ -15,8 +12,8 @@ def test_html():
 	Title: Test Html
 	URL: site/test_html
 	Method: GET
-    """
-    
+	"""
+
 	webPage = "<html><body><center>this is supercool db!</center></body></html>"
 
 	cfunc=test_html
@@ -25,14 +22,15 @@ def test_html():
 
 	return webPage
 
+
 @Site_Main_Flask_Obj.route('/site/main',methods=['POST','GET'])
 def main_html():
 	"""
 	Title: Test Html
 	URL: site/main_html
 	Method: GET
-    """
-    
+	"""
+
 	webPage = ""
 	webPage += "<html>"
 	webPage += "<title>Seqeunce Search</title>"
@@ -47,9 +45,10 @@ def main_html():
 
 	cfunc=test_html
 	if request.method=='POST':
-	   return(getdoc(cfunc))
+		return(getdoc(cfunc))
 
 	return webPage
+
 
 @Site_Main_Flask_Obj.route('/site/search_results',methods=['POST'])
 def search_results():
@@ -57,49 +56,49 @@ def search_results():
 	Title: Search results page
 	URL: site/search_results
 	Method: POST
-    """
+	"""
 
 	sequence = request.form['sequence']
-    
+
 	style = "<style>table {margin:40px; border-collapse: collapse; width: 100%;} th, td {text-align: left; padding: 8px;}tr:nth-child(even){background-color: #f2f2f2}th {background-color: #4CAF50;color: white; margin-top:100px;}</style>"
 
 	webPage = ""
 	webPage += "<html>"
 	webPage += "<title>Seqeunce Search</title>"
 	webPage += "<head>" + style + "</head>"
-	webPage += "<body>Search results for sequence:" +  sequence + "<br>"
-    
+	webPage += "<body>Search results for sequence:" + sequence + "<br>"
+
 	rdata = {}
-	rdata['sequence'] = sequence 
+	rdata['sequence'] = sequence
 	httpRes=requests.get('http://127.0.0.1:5000/sequences/get_annotations',json=rdata)
-	
+
 	if httpRes.status_code != requests.codes.ok:
-	   debug(6,"Error code:" + str(httpRes.status_code))
-	   webPage += "Failed to get annotations for the given sequence"
+		debug(6,"Error code:" + str(httpRes.status_code))
+		webPage += "Failed to get annotations for the given sequence"
 	else:
-	   jsonResponse = httpRes.json()
-	   webPage += "<table>"   
-	   webPage += "<col width='10%'>"
-	   webPage += "<col width='30%'>"
-	   webPage += "<col width='60%'>"
-	   webPage += "<tr>" 
-	   webPage += "<th>Expirment id</th>"
-	   webPage += "<th>Description</th>"
-	   webPage += "<th>Details</th>"
-	   webPage += "</tr>" 
-	   strDetails = "" 
-	   for dataRow in jsonResponse.get('annotations'):
-	       webPage += "<tr>" 
-	       webPage += "<td><a href="">" + str(dataRow.get('expid','not found')) + "</a></td>"
-	       webPage += "<td>" + str(dataRow.get('description','not found')) + "</td>"
-	       #webPage += "<td>" + str(dataRow) + "</td>"
-	       strDetails = ''
-	       for detailesRow in dataRow.get('details'):    
-                strDetails += str(detailesRow)
-	       webPage += "<td>" + str(strDetails) + "</td>"
-	       webPage += "</tr>" 
-	   webPage += "</table>"     
+		jsonResponse = httpRes.json()
+		webPage += "<table>"
+		webPage += "<col width='10%'>"
+		webPage += "<col width='30%'>"
+		webPage += "<col width='60%'>"
+		webPage += "<tr>"
+		webPage += "<th>Expirment id</th>"
+		webPage += "<th>Description</th>"
+		webPage += "<th>Details</th>"
+		webPage += "</tr>"
+		strDetails = ""
+		for dataRow in jsonResponse.get('annotations'):
+			webPage += "<tr>"
+			webPage += "<td><a href="">" + str(dataRow.get('expid','not found')) + "</a></td>"
+			webPage += "<td>" + str(dataRow.get('description','not found')) + "</td>"
+			#webPage += "<td>" + str(dataRow) + "</td>"
+			strDetails = ''
+			for detailesRow in dataRow.get('details'):
+				strDetails += str(detailesRow)
+			webPage += "<td>" + str(strDetails) + "</td>"
+			webPage += "</tr>"
+		webPage += "</table>"
 	webPage += "</body>"
 	webPage += "</html>"
-    
+
 	return webPage
