ef7c87499373397ed8012b317a7d772cec78a86f
amnona <amnonim@gmail.com>
2017-10-03 15:50:01 +0300

    relative freqs for single sequence

--- scdb-website/Site_Main_Flask.py
+++ scdb-website/Site_Main_Flask.py
@@ -1297,7 +1297,11 @@ def get_term_info_for_annotations(annotations):
     The statistics about each term
     '''
     terms = get_annotations_terms(annotations)
+    print('get term info')
+    print(terms)
     res = requests.get(get_db_address() + '/ontology/get_term_stats', json={'terms': terms})
+    print('res is')
+    print(res)
     # ans = res.json()
     # term_info = ans.get('term_info')
     # return term_info
